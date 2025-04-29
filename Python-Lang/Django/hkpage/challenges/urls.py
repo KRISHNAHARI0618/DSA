c56af1be1186bd24c781c816bb99e4d7fb3b6e77
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-04-29 10:00:04 +0000

    29-04-2025 10:00:01

--- Python-Lang/Django/hkpage/challenges/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("day1", views.trigger1),
-    path("day2",views.trigger2),
-    path("day3",views.trigger3),
-    path("day4",views.trigger4,name='trigger4')
-]
