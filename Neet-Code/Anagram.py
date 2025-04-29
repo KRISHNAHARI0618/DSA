ef33797b2db730f80e33c9dad95846ddab0af605
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-04-29 01:28:08 +0000

    29-04-2025 01:27:56

---
+++ Neet-Code/Anagram.py
@@ -0,0 +1,44 @@
+from collections import Counter
+from syslog import LOG_KERN
+
+
+class Anagram:
+    def anagrams(self,s:str,c:str)-> bool:
+        # return Counter(s) == Counter(c)
+        countS ,countT = {}, {}
+        if len(s) != len(c):
+            return False
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[c[i]] = 1 + countT.get(c[i],0)
+        for c in countS:
+            if countS[c] != countT.get(c,0):
+                return False
+        return True 
+ 
+sol = Anagram()
+res = sol.anagrams("anagram","nagraam")
+print(res)
+
+def countChars(n:str):
+    dicts = {}
+    for i in range(len(n)):
+        dicts[n[i]] = 1 + dicts.get(n[i],0)
+        print(dicts[n[i]])
+
+result = countChars("peddireddy")
+
+
+def anagramsCount(s:str,c:str):
+    dictS,dictC = {},{}
+    if len(s) != len(c):
+        return False
+    for i in range(len(s)):
+        dictS[s[i]] = 1 + dictS.get(s[i],0) # a = 1 b = 1 c = 1 a = 2 b = 2 c = 2
+        dictC[c[i]] = 1 + dictC.get(c[i],0) # b = 1 b = 2 c = 1 c = 2 a = 1 a = 2
+    for c in dictS:
+        if dictS[c] != dictC.get(c,0):
+            return False
+    return True
+
+print(anagramsCount("abcabc","bbccaa"))
---
+++ Neet-Code/Contains-Duplicate.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+
+class solution:
+    def containsDuplicate(self,nums: list[int]) -> Any:
+        hashSet = set()
+        for i in nums:
+            if i in hashSet:
+                return True
+            hashSet.add(i)
+        return False
+    
+sol = solution()
+arr = [2,3,9,8,6,5]
+result = sol.containsDuplicate(arr)
+print(result)
+
+
+def containsDuplicates(name: Any)-> Any:
+    hashSet = set()
+    for i in range(len(name)):
+        hashSet.add(name[i])
+    return hashSet
+        
+output = containsDuplicates("peddireddy")
+print(output)
+
+def containsDup(arr: Any) -> Any:
+    hashSet = set()
+    dupSet = set()
+    for i in arr:
+        if i in hashSet:
+            dupSet.add(i)
+        else:
+            hashSet.add(i)
+    return hashSet,dupSet
+
+list1 = [2,3,9,8,6,5,2,3]
+result1,result2 = containsDup(list1)
+print(result1,result2)
