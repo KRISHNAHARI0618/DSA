7633e7ee067bc0dde600ea67cf2da51bc405a5d0
KRISHNAHARI0618 <harivardhan9397921834@gmail.com>
2025-05-01 12:19:54 +0000

    01-May-2025

--- DevOps/Shell-Scripts/06-Functions.sh
+++ DevOps/Shell-Scripts/06-Functions.sh
@@ -5,6 +5,8 @@ dates=$(date +%F)
 
 echo "$dates"
 
+echo "$0"
+
 validate(){
     # Validate will take input as first argument while calling 
     if [ $1 -ne 0 ]
@@ -26,3 +28,13 @@ rm -rf "$date".txt
 apt install git -y &>> "$date".txt
 
 validate $? 1 "Git"
+
+echo -e "\e[30mHello World"
+echo -e "\e[31mHello World"
+echo -e "\e[32mHello World"
+echo -e "\e[33mHello World"
+echo -e "\e[34mHello World"
+echo -e "\e[35mHello World"
+echo -e "\e[36mHello World"
+echo -e "\e[37mHello World"
+
