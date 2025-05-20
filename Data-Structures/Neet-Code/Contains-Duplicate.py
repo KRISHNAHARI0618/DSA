ea00fb8809f3140f75cb4dde92391b928135c7a4
krishnahari0618 <harivardhan9397921834@gmail.com>
2025-05-20 22:14:34 +0530

    Auto-sync: 2025-05-20 22:14:33

--- Data-Structures/Neet-Code/Contains-Duplicate.py
+++ Data-Structures/Neet-Code/Contains-Duplicate.py
@@ -1,18 +1,33 @@
-# Contains Duplicate
+# Contains Duplicate Hash Set Method
 
 class solution:
     def containsDuplicate(self,nums:list[int]):
         hashSet = set()
         for i in nums:
-            hashSet.add(i)
             if i in hashSet:
                 return True                
+            hashSet.add(i)
         return hashSet
 
 obj1 = solution()
 arrs = [1,2,3,2,1,3,6,9,7,6,9,5]
 numsd = obj1.containsDuplicate(arrs)
-
+n = len(arrs)
 print(numsd)
 
 
+
+def containsduplicate(numbers):
+    return numbers != set(numbers)
+
+if containsduplicate(arrs):
+    print(True)
+else:
+    print(False)
+
+
+nums = [1,2,3,4,1]
+if nums != set(nums):
+    print(True)
+else:
+    print(False)
